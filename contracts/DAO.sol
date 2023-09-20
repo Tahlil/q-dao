62a2aa7f2d5e1c0a6b24a2edad8b6321b8f9fce6
Tahlil <bsse0803@iit.du.ac.bd>
2023-09-20 22:53:58 +0600

    Add member addition functionality

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -28,6 +28,18 @@ contract DAO {
     event ProposalAccepted(string message);
     event ProposalRejected(string rejected);
 
+    function addMember(address _member) public {
+      require(memberInfo[_member].memberAddress == address(0), "Member already exists");
+      memberInfo[_member] = Member({
+          memberAddress: _member,
+          memberSince: block.timestamp,
+          tokenBalance: 100
+      });
+      members.push(_member);
+      balances[_member] = 100;
+      totalSupply += 100;
+    }
+
 }
 
 
