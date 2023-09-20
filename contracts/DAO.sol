da9525627a271ad4ecc9f113b95a29fd0f60f986
Tahlil <bsse0803@iit.du.ac.bd>
2023-09-20 22:53:23 +0600

    Add events

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -23,6 +23,11 @@ contract DAO {
     uint public totalSupply;
     mapping(address => uint) public balances;
 
+    event ProposalCreated(uint indexed proposalId, string description);
+    event VoteCast(address indexed voter, uint indexed proposalId, uint tokenAmount);
+    event ProposalAccepted(string message);
+    event ProposalRejected(string rejected);
+
 }
 
 
